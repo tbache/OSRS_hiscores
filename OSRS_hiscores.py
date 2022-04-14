b30afb13f3dc7db01accaaf1797901d65874e2fe
Tom Bache <43818735+tbache@users.noreply.github.com>
2022-04-14 11:33:38 +0100

    Script doc.

--- OSRS_hiscores.py
+++ OSRS_hiscores.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Apr 14 11:16:50 2022
+Script that obtains data from OSRS hiscores and displays interesting
+information.
 
+Created on Thu Apr 14 11:16:50 2022
 @author: Tom Bache
 """
