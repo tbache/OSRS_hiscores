a8e22424326595bb88ab6505ad76fb00f207f88b
Tom Bache <43818735+tbache@users.noreply.github.com>
2022-04-14 11:59:03 +0100

    Read player name from CL and obtain stats from webpage.

--- OSRS_hiscores.py
+++ OSRS_hiscores.py
@@ -5,3 +5,27 @@ information.
 Created on Thu Apr 14 11:16:50 2022
 @author: Tom Bache
 """
+
+import argparse
+import pandas as pd
+
+# Required when running in some IDEs
+from urllib.request import Request, urlopen
+
+
+if __name__ == '__main__':
+
+    # Parse CL arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--player', nargs=1, default='ioniph',
+        help='Name of the player whose stats are to be fetched.')
+    args = parser.parse_args()
+
+    # Read players stats from OSRS hiscores
+    player = str(args.player)
+    req = Request('https://secure.runescape.com/m=hiscore_oldschool/hiscorepersonal?user1='+player,
+                  headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    hiscores = pd.read_html(webpage)
+    print(hiscores)
