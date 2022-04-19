19095e11d4a2b683f5502d5e0a5ec977e546f80f
Tom Bache <43818735+tbache@users.noreply.github.com>
2022-04-19 09:50:19 +0100

    Legend added to skills plots.

--- OSRS_hiscores.py
+++ OSRS_hiscores.py
@@ -14,6 +14,7 @@ from datetime import datetime
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.patches
 # Required when running in some IDEs (e.g. Spyder):
 from urllib.request import Request, urlopen
 
@@ -231,20 +232,29 @@ if __name__ == '__main__':
     # print(skills)
     # print(killcount)
 
-    # Plot skills
+    # Plot skills (one skill per plot)
     skills_plot = sns.FacetGrid(data=skills, col='Skill', col_wrap=4,
                                 sharey=False, height=3.5, aspect=1.5)
     skills_plot.map(sns.lineplot, 'Date', 'XP')
+    # Add plot of level to XP plot using a 2nd y-axis
     for ax, (_, subdata) in zip(skills_plot.axes, skills.groupby('Skill', sort=False)):
         ax2 = ax.twinx()
         subdata.plot(x='Date', y='Level', ax=ax2, legend=False, color='b')
         ax.set_ylabel('XP')
         ax2.set_ylabel('Level')
+
+        # Create legend (colours obtained via plt.gca().lines[-1].get_color())
+        name_to_color = {
+            'XP': (0.8862745098039215, 0.2901960784313726, 0.2),
+            'Level': 'b'
+        }
+        patches = [matplotlib.patches.Patch(
+            color=v, label=k) for k, v in name_to_color.items()]
+        plt.legend(handles=patches, loc='upper left')
     RotateTickLabels(skills_plot)
-    skills_plot.add_legend()
     plt.tight_layout()
 
-    # Plot killcount
+    # Plot killcount (one boss per plot)
     killcount_plot = sns.FacetGrid(data=killcount, col='Boss', col_wrap=6,
                                    sharey=False)
     killcount_plot.map(sns.lineplot, 'Date', 'Kill count')
